7c655c326c5b4195e1e0a143224f93de954145fd
toniju98 <33891402+toniju98@users.noreply.github.com>
2023-12-18 14:49:16 +0100

    update

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,64 +5,66 @@ import Logo from "../assets/Logo.svg";
 function Footer() {
   return (
     <footer className="app__footer">
-      <img src={Logo}/>
-      <div className="app__footer-navigation">
-        <h2 className="app__footer-header">Navigation</h2>
-        <ul className="app__footer-subtext">
-        <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/menu">Menu</a>
-          </li>
-          <li>
-            <a href="/reservations">Reservations</a>
-          </li>
-          <li>
-            <a href="/order-online">Order online</a>
-          </li>
-          <li>
-            <a href="/login">Login</a>
-          </li>
-        </ul>
-      </div>
+      <img src={Logo} />
+      <div className="app__footer-textMenu">
+        <div className="app__footer-navigation">
+          <h2 className="app__footer-header">Navigation</h2>
+          <ul className="app__footer-subtext">
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+              <a href="/reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="/order-online">Order online</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
 
-      <div className="app__footer-navigation">
-        <h2 className="app__footer-header">Contact</h2>
-        <ul className="app__footer-subtext">
-        <li>
-            <a href="/home">Address</a>
-          </li>
-          <li>
-            <a href="/about">Phone Number</a>
-          </li>
-          <li>
-            <a href="/menu">Email</a>
-          </li>
-        </ul>
-      </div>
+        <div className="app__footer-navigation">
+          <h2 className="app__footer-header">Contact</h2>
+          <ul className="app__footer-subtext">
+            <li>
+              <a href="/home">Address</a>
+            </li>
+            <li>
+              <a href="/about">Phone Number</a>
+            </li>
+            <li>
+              <a href="/menu">Email</a>
+            </li>
+          </ul>
+        </div>
 
-      <div className="app__footer-navigation">
-        <h2 className="app__footer-header">Social media links</h2>
-        <ul className="app__footer-subtext">
-        <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/menu">Menu</a>
-          </li>
-          <li>
-            <a href="/reservations">Reservations</a>
-          </li>
-        </ul>
+        <div className="app__footer-navigation">
+          <h2 className="app__footer-header">Social media links</h2>
+          <ul className="app__footer-subtext">
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+              <a href="/reservations">Reservations</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      </footer>
+    </footer>
   );
 }
 
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,30 +1,66 @@
 import "./Navbar.css";
 import Logo from "../assets/Logo.svg";
+import React from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
     <nav className="app__navbar">
       <img src={Logo} />
-        <ul className="app__navbar-navlinks">
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/menu">Menu</a>
-          </li>
-          <li>
-            <a href="/reservations">Reservations</a>
-          </li>
-          <li>
-            <a href="/order-online">Order online</a>
-          </li>
-          <li>
-            <a href="/login">Login</a>
-          </li>
-        </ul>
+      <ul className="app__navbar-navlinks">
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="/order-online">Order online</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+      <div className="app__navbar-smallscreen">
+        <AiOutlineMenu
+          color="#000000"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <AiOutlineClose
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li>
+                <a href="/" onClick={() => setToggleMenu(false)}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/about-us" onClick={() => setToggleMenu(false)}>
+                  Über uns
+                </a>
+              </li>
+              <li>
+                <a href="/contact" onClick={() => setToggleMenu(false)}>
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
